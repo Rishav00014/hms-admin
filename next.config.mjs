7267f1ae6f2c8102186ecdc329ecd428c00b3ac1
Rishav Kumar <rishav8541037942@gmail.com>
2025-01-28 09:50:20 +0530

    added base url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BACKEND_API_BASE_URL:"https://api.hisecureexhibitions.com/",
+    BACKEND_API_BASE_URL:"https://hms-api.devloperhemant.com/",
   },
   images: {
     domains: ["img.freepik.com"],
